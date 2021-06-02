1faec9847726c7c13f5f440cf828c45b91297cca
SergyDany47 <sergiodani47@gmail.com>
2021-06-02 22:16:03 +0200

    Terminado PageFragment1 y componentes Bar

--- app/src/main/java/com/sap/dryice/LoginActivity.java
+++ app/src/main/java/com/sap/dryice/LoginActivity.java
@@ -2,7 +2,9 @@ package com.sap.dryice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void loginToRegisterSidebarClick(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+    public void loginToGraphicActivity(View view) {
+        startActivity(new Intent(this, ActivityGraphics.class));
+        overridePendingTransition(R.anim.slide_in_top, R.anim.stay);
     }
 }
--- app/src/main/java/com/sap/dryice/SplashActivity.java
+++ app/src/main/java/com/sap/dryice/SplashActivity.java
@@ -43,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashActivity
-                        .this, ActivityGraphics.class);
+                        .this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
