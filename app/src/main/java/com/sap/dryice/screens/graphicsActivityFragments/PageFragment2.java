a48d4b4be690b86f81290419e4eb93bf4dee6213
Albersss <alber.garcia.lopez106@gmail.com>
2021-06-03 12:33:38 +0200

    First chart implemented. Charts is love

--- app/src/main/java/com/sap/dryice/screens/graphicsActivityFragments/PageFragment2.java
+++ app/src/main/java/com/sap/dryice/screens/graphicsActivityFragments/PageFragment2.java
@@ -1,23 +1,130 @@
 package com.sap.dryice.screens.graphicsActivityFragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.sap.dryice.R;
 
+import java.util.ArrayList;
+
 public class PageFragment2 extends Fragment {
+
+    private BarChart barChart;
+    private String[] datos = new String[]{"C02 Máximo", "C02 Mínimo"};
+    private int[] datosRasp = new int[]{305, 21};
+    private int[] datosColores = new int[]{Color.rgb(0,255,255), Color.rgb(252,100,2)};
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.main_page2, container, false);
 
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.main_page2,container,false);
+        barChart = (BarChart) rootView.findViewById(R.id.barChart);
+        createCharts();
+
+
         return rootView;
+
     }
+
+
+    private Chart getChart(Chart chart, String description, int textColor, int background, int animateY) {
+        chart.getDescription().setText(description);
+        chart.getDescription().setTextSize(15);
+        chart.setBackgroundColor(background);
+        chart.animateY(animateY);
+        legend(chart);
+        return chart;
+    }
+
+    private void legend(Chart chart) {
+        Legend legend = chart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        ArrayList<LegendEntry> entries = new ArrayList<>();
+        for (int i = 0; i < datosRasp.length; i++) {
+            LegendEntry entry = new LegendEntry();
+            entry.formColor = datosColores[i];
+            entry.label = datos[i];
+            entries.add(entry);
+        }
+        legend.setCustom(entries);
+    }
+
+    private ArrayList<BarEntry> getBarEntries() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i = 0; i < datosRasp.length; i++) {
+            entries.add(new BarEntry(i, datosRasp[i]));
+        }
+        return entries;
+    }
+
+    private void axisX(XAxis axis) {
+        axis.setGranularityEnabled(true);
+        axis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        axis.setValueFormatter(new IndexAxisValueFormatter(datos));
+        axis.setEnabled(false);
+    }
+
+    private void axisLeft(YAxis axis) {
+        axis.setSpaceTop(30);
+        axis.setAxisMinimum(0);
+    }
+
+    private void axisRight(YAxis axis) {
+        axis.setEnabled(false);
+    }
+
+    private void createCharts() {
+        barChart = (BarChart) getChart(barChart, "", Color.GRAY, Color.rgb(76, 80, 118), 3000);
+        //barChart.setDrawGridBackground(false);
+        //barChart.setDrawBarShadow(false);
+        barChart.setData(getBarData());
+        barChart.invalidate();
+        axisX(barChart.getXAxis());
+        axisLeft(barChart.getAxisLeft());
+        axisRight(barChart.getAxisRight());
+    }
+
+    private DataSet getData(DataSet dataSet) {
+        dataSet.setColors(datosColores);
+        dataSet.setValueTextSize(Color.WHITE);
+        dataSet.setValueTextSize(10);
+        return dataSet;
+    }
+
+    private BarData getBarData() {
+        BarDataSet barDataSet = (BarDataSet) getData(new BarDataSet(getBarEntries(), ""));
+        barDataSet.setBarShadowColor(Color.DKGRAY);
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.45f);
+        return barData;
+    }
+
 }
+
